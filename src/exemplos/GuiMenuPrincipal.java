888b875e5689b0c6782f7c74af492f1b9af05231
a9210103 <a9210103@02LAB108PC05.rn.senac.br>
2016-07-19 21:26:12 -0300

    escola

--- src/exemplos/GuiMenuPrincipal.java
+++ src/exemplos/GuiMenuPrincipal.java
@@ -5,86 +5,113 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GuiMenuPrincipal extends JFrame{
-
+   
     private Container contentPane;
     private JMenuBar mnBarra;
     private JMenu mnArquivo, mnExemplos;
-    private JMenuItem miSair, miBotao, miCaixaOpcao;
-    
+    private JMenuItem miSair, miBotao,miCaixaOpcao;
+    private JMenuItem miRadio;
+    private JMenuItem miLista; miFoto;
+   
     public GuiMenuPrincipal(){
         inicializarComponentes();
-        definirEventos();
+        definirEventos();       
+    }    
+   
+    private void inicializarComponentes(){
+        setTitle("Menu principal");
+        setBounds(0,0,800,600);
+        contentPane = getContentPane();
+       
+        mnBarra = new JMenuBar();
+        mnArquivo = new JMenu("Arquivo");
+        mnExemplos = new JMenu("Exemplos");
+        miSair = new JMenuItem("Sair");
+        miBotao = new JMenuItem("Botao");
+        miCaixaOpcao = new JMenuItem("Caixa de Opção");
+        miRadio = new JMenuItem("Botão de Rádio");
+        miLista = new JMenuItem("Lista");
+        miListaFoto = new JMenuItem("Foto");
+       
+        mnArquivo.setMnemonic('A');
+        mnExemplos.setMnemonic('E');
+       
+        mnArquivo.add(miSair);
+        mnExemplos.add(miBotao);
+        mnExemplos.add(miCaixaOpcao);
+        mnExemplos.add(miRadio);
+        mnExemplos.add(miLista);
+        mnExemplos.add(miFoto);
+        mnBarra.add(mnArquivo);
+        mnBarra.add(mnExemplos);
+        setJMenuBar(mnBarra);
     }
-    
-private void inicializarComponentes(){
-    setTitle("Menu Principal");
-    setBounds(0,0,800,600);
-    contentPane = getContentPane();
-    
-    mnBarra = new JMenuBar();
-    mnArquivo = new JMenu("Arquivo");
-    mnExemplos = new JMenu("Exemplos");
-    miSair = new JMenuItem("SAIR");
-    miBotao = new JMenuItem("BOTÃO");
-    miCaixaOpcao = new JMenuItem("Caixa de Opcao");
-    
-    mnArquivo.setMnemonic('A');
-    mnExemplos.setMnemonic('E');
-    
-    mnArquivo.add(miSair);
-    mnExemplos.add(miBotao);
-    mnExemplos.add(miCaixaOpcao);
-    mnBarra.add(mnArquivo);
-    mnBarra.add(mnExemplos);
-    setJMenuBar(mnBarra);
-    
-    
-}
-
+   
     private void definirEventos(){
         miSair.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
+       
+       
         miBotao.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 GuiBotao botao = new GuiBotao();
                 contentPane.removeAll();
                 contentPane.add(botao);
                 contentPane.validate();
-            
             }
         });
+       
+        miRadio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GuiRadio radio = new GuiRadio();
+                contentPane.removeAll();
+                contentPane.add(radio);
+                contentPane.validate();
+            }
+        });
+        miLista.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GuiLista lista = new GuiLista();
+                contentPane.removeAll();
+                contentPane.add(lista);
+                contentPane.validate();
+            }
+        });
+        miListaFoto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GuiListaFoto Foto = new GuiLista();
+                contentPane.removeAll();
+                contentPane.add(Foto);
+                contentPane.validate();
+            }
+        });
+       
         miCaixaOpcao.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 GuiCaixaOpcao opcao = new GuiCaixaOpcao();
                 contentPane.removeAll();
-                contentPane.add(opcao);
+                contentPane.add(opcao); 
                 contentPane.validate();
             }
         });
-        
     }
-    
+   
     public static void run(){
         GuiMenuPrincipal frame = new GuiMenuPrincipal();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (tela.width - frame.getSize().width) / 2;
-        int y = (tela.height - frame.getSize().height) / 2;
+        int x = (tela.width - frame.getSize().width)/2;
+        int y = (tela.height - frame.getSize().height)/2;
         frame.setLocation(x,y);
-        frame.setVisible(true);      
-        
-    }
-    
-    
-    
-    
+        frame.setVisible(true);
+    }  
 }
